90f67e3e6942f799ce4d39e8f452062182d112d9
DomHuf1 <domhu@yahoo.com>
2020-07-23 22:15:57 +0800

    move file location

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './main.js',
+   entry: './src/main.js',
    output: {
       path: path.join(__dirname, '/bundle'),
       filename: 'index_bundle.js'
@@ -25,7 +25,7 @@ module.exports = {
    },
    plugins:[
       new HtmlWebpackPlugin({
-         template: './index.html'
+         template: './public/index.html'
       })
    ]
 }
